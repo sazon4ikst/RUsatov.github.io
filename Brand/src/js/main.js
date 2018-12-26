584991b3dacaf455b6616a75fe7c7cdcb3a2f8dd
Roman Usatov <R.Usatov76@gmail.com>
2018-12-26 20:20:41 +0300

    add Brand Fashion

---
+++ Brand/src/js/main.js
@@ -0,0 +1,14 @@
+/*
+ * Third party
+ */
+
+//= ../../bower_components/jquery/dist/jquery.js
+//= ../../bower_components/bootstrap/dist/js/bootstrap.js
+//= ../../bower_components/swiper/dist/js/swiper.min.js
+
+
+/*
+ * Custom
+ */
+
+//= partials/app.js
---
+++ Brand/src/js/partials/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  //initialize swiper when document ready
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    
+  })
+});
